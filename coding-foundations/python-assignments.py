07ef02c81a3dae6b228b22a7c56f50575248af0e
PastelPrism <amy-van-leeuwen@proton.me>
2025-07-06 22:04:56 +0200

    first 7 assignments for coding-foundations

---
+++ coding-foundations/python-assignments.py
@@ -0,0 +1,69 @@
+# Game Messages - Display 'Game Over' on the screen 
+
+message = "Game over"
+message = print(message)
+
+# Shopping Prices - Create a variable to store numerical value and display it on screen
+
+price = 139
+print(price)
+
+# The Semester Grade - Display expected result (133) on the screen 
+
+midterm = 55
+final = 78
+semester_grade = midterm + final
+print(semester_grade)
+
+# And The Winner Is ... - Fix the bugs
+
+name = "Anna"
+score = 96
+print(name)
+print(score)
+
+# Flight Tracker - Make use of comments 
+
+# flight number
+flight_n = "BA0117"
+
+# destination of the flight
+destination = "New York"
+
+# istance to the destination
+distance = 1580
+
+# Displays flight number
+print(flight_n)
+
+# Displays destination
+print(destination)
+
+# Displays distance
+print(distance)
+
+# Snowflake - Debug the code 
+
+p1 = "  *  "
+p2 = " *** "
+p3 = "*****"
+
+print(p1)
+print(p2)
+print(p3)
+print(p2)
+print(p1)
+
+#It's payday! - Expected result: 160000
+
+salary = 150000
+pay_raise = 10000
+
+new_salary = salary + pay_raise
+
+
+print(new_salary)
+
+
+
+
